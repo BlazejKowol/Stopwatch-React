6dd7e71708cc1093c77037b53c8fffe3e8a118e9
Błażej Kowol <kowol.blazej@gmail.com>
2023-01-27 23:37:53 +0100

    add styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import styles from './components/Button/Button.module.scss';
+
 const App = () => {
   return (
     <div>
-      <h1>00:00:00.0</h1>
+      <h1>00:00:00.0</h1> {/*tu będzie komponent Timer */}
       <container> {/*tu będzie komponent ze wszystkimi buttonami */}
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        <button className={styles.button}>Start</button>
+        <button className={styles.button}>Stop</button>
+        <button className={styles.button}>Reset</button>
       </container>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-//import './styles/normalize.scss';
-//import './styles/global.scss';
+import './styles/normalize.scss';
+import './styles/global.scss';
 
 
 ReactDOM.render(
