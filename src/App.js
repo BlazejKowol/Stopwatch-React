7d96cf977463bf680a96f07ff5b543e5415574ba
Błażej Kowol <kowol.blazej@gmail.com>
2023-01-27 22:44:43 +0100

    Initial commit with first code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>00:00:00.0</h1>
+      <container> {/*tu będzie komponent ze wszystkimi buttonami */}
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </container>
     </div>
   );
 }
